997d2eeeb32f862c93e4a3b4ba0a6637fe1fb2b9
Hikaru Gyoji <49551505+gstein314@users.noreply.github.com>
2022-06-22 14:39:45 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,7 @@ export default {
     },
   },
 
-  router: {
-    base: process.env.NODE_ENV === 'dev' ? '/' : '/dbcls/refex/',
-  },
+  //router: {
+  //  base: process.env.NODE_ENV === 'dev' ? '/' : '/dbcls/refex/',
+  //},
 };
